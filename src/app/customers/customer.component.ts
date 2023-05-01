7fc01600bb15bfe06f57bd2d2960f6f008ce99ce
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-05-01 22:09:04 +0530

    simplifying with a FormBUILder

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -12,17 +12,26 @@ export class CustomerComponent implements OnInit {
   customerForm!: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
+//Data inizializing using life cyle hock
   ngOnInit(): void {
-    this.customerForm=new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true),
-    });
+    //creating a Form group method using Formbuilder instance,FoemControl associated with the Form group.
+    this.customerForm=this.fb.group({
+      firstName:'',
+      lastName:'',
+      email:'',
+      sendCatalog: true
+    })
+  
+  //   this.customerForm=new FormGroup({
+  //     firstName: new FormControl(),
+  //     lastName: new FormControl(),
+  //     email: new FormControl(),
+  //     sendCatalog: new FormControl(true),
+  //   });
   }
-
+// Data get form a component to html
   populateTestData():void{
     this.customerForm.setValue({
       firstName:'Jack',
@@ -40,7 +49,7 @@ export class CustomerComponent implements OnInit {
   //     sendCatalog:false
   //   });
   // }
-
+//this is the method passing we click a button in UI
   save(): void {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
