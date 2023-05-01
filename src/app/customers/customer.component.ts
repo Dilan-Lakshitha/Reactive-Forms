71f60ace2970a2114310bee28e27ca448db9dff8
Dilan-Lakshitha <lakshithadilan6@gmail.com>
2023-05-02 00:55:25 +0530

    validatiob rule at runtime

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -12,32 +12,34 @@ export class CustomerComponent implements OnInit {
   customerForm!: FormGroup;
   customer = new Customer();
 
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb: FormBuilder) { }
 
-//Data inizializing using life cyle hock
+  //Data inizializing using life cyle hock
   ngOnInit(): void {
     //creating a Form group method using Formbuilder instance,FoemControl associated with the Form group.
-    this.customerForm=this.fb.group({
-      firstName:['',[Validators.minLength(3)]],
-      lastName:['',[Validators.minLength(3)]],
-      email:'',
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.minLength(3)]],
+      lastName: ['', [Validators.minLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
       sendCatalog: true
     })
-  
-  //   this.customerForm=new FormGroup({
-  //     firstName: new FormControl(),
-  //     lastName: new FormControl(),
-  //     email: new FormControl(),
-  //     sendCatalog: new FormControl(true),
-  //   });
+
+    //   this.customerForm=new FormGroup({
+    //     firstName: new FormControl(),
+    //     lastName: new FormControl(),
+    //     email: new FormControl(),
+    //     sendCatalog: new FormControl(true),
+    //   });
   }
-// Data get form a component to html
-  populateTestData():void{
+  // Data get form a component to html
+  populateTestData(): void {
     this.customerForm.setValue({
-      firstName:'Jack',
-      lastName:'Harkness',
-      email:'jack@torchwood.com',
-      sendCatalog:false
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      email: 'jack@torchwood.com',
+      sendCatalog: false
     });
   }
 
@@ -49,9 +51,20 @@ export class CustomerComponent implements OnInit {
   //     sendCatalog:false
   //   });
   // }
-//this is the method passing we click a button in UI
+  //this is the method passing we click a button in UI
   save(): void {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  //validation rule that adjusts itself at runtime wen click a button 
+  setNotifaction(notifiy: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifiy == 'text') {
+      phoneControl?.setValidators(Validators.required);
+    } else {
+      phoneControl?.clearValidators();
+    }
+    phoneControl?.updateValueAndValidity();
   }
 }
